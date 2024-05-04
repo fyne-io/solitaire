f03ec537d3d4062eb7ca5c9d4389fae2e656b5c7
Andy Williams <andy@andy.xyz>
2024-05-04 17:49:17 +0100

    Theme tweaks and images now we're looking good

--- theme.go
+++ theme.go
@@ -15,7 +15,7 @@ func newGameTheme() fyne.Theme {
 	return &gameTheme{theme.DefaultTheme()}
 }
 
-func (g *gameTheme) Color(n fyne.ThemeColorName, v fyne.ThemeVariant) color.Color {
+func (g *gameTheme) Color(n fyne.ThemeColorName, _ fyne.ThemeVariant) color.Color {
 	switch n {
 	case theme.ColorNameBackground:
 		return color.RGBA{R: 0x07, G: 0x63, B: 0x24, A: 0xff}
@@ -23,5 +23,5 @@ func (g *gameTheme) Color(n fyne.ThemeColorName, v fyne.ThemeVariant) color.Colo
 		return color.RGBA{R: 0x02, G: 0x52, B: 0x10, A: 0xff}
 	}
 
-	return g.Theme.Color(n, v)
+	return g.Theme.Color(n, theme.VariantDark)
 }
